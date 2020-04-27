5ab23d3fa59aae18efa228a4fa26c63fdabd411b
Kyle Jones <mynameiskyle242@gmail.com>
2020-04-27 19:55:21 -0400

    Fundamentals of React - React Component Definition (Advanced)

--- src/App.js
+++ src/App.js
@@ -19,31 +19,26 @@ const list = [
     },
 ];
 
-function App() {
-    return (
-        <div>
-            <h1>My Hacker Stories</h1>
-            <label htmlFor="search">Search: </label>
-            <input id="search" type="text" />
-            <hr />
-            <List />
-        </div>
-    );
-}
+const App = () => (
+    <div>
+        <h1>My Hacker Stories</h1>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" />
+        <hr />
+        <List />
+    </div>
+);
 
-function List() {
-    return list.map(function(item) {
-        return (
-            <div key={item.objectID}>
-                <span>
-                    <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-            </div>
-        );
-    });
-}
+const List = () =>
+    list.map(item => (
+        <div key={item.objectID}>
+            <span>
+                <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+        </div>
+    ));
 
 export default App;
