d3afd2a4a3d8495b1a2c7fa26cb44abea92bfef2
Kyle Jones <kyleajones242@gmail.com>
2020-04-26 21:40:39 -0400

    Fundamentals of React - React JSX

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function getTitle(title) {
+    return title;
+}
+function App() {
+    return (
+        <div>
+            <h1>Hello {getTitle('React')}</h1>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
+        </div>
+    );
+}
+export default App;
