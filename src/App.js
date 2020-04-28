8155628fe556cdbf8db1e32dabe33b4b80d9cafa
Kyle Jones <mynameiskyle242@gmail.com>
2020-04-28 19:04:10 -0400

    Fundamentals of React - React Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 
-const list = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
-
 const App = () => {
+    const stories = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org/',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
     const handleChange = event => {
         console.log(event);
     };
@@ -31,17 +30,17 @@ const App = () => {
             <input id="search" type="text" onChange={handleChange} />
             <hr />
 
-            <List />
+            <List list={stories} />
         </div>
     );
 };
 
-const List = () =>
-    list.map(item => (
+const List = props =>
+    props.list.map(item => (
         <div key={item.objectID}>
-            <span>
-                <a href={item.url}>{item.title}</a>
-            </span>
+        <span>
+            <a href={item.url}>{item.title}</a>
+        </span>
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
