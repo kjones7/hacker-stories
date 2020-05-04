a15d930c3bab29298e9dbb1be457b60096fe0386
Kyle Jones <mynameiskyle242@gmail.com>
2020-05-04 18:51:31 -0400

    Fundamentals of React - React Component Composition

--- src/App.js
+++ src/App.js
@@ -49,10 +49,11 @@ const App = () => {
 
             <InputWithLabel
                 id="search"
-                label="Search"
                 value={searchTerm}
                 onInputChange={handleSearch}
-            />
+            >
+                <strong>Search:</strong>
+            </InputWithLabel>
 
             <hr />
 
@@ -63,13 +64,13 @@ const App = () => {
 
 const InputWithLabel = ({
     id,
-    label,
     value,
     type = 'text',
     onInputChange,
+    children,
 }) => (
     <>
-        <label htmlFor={id}>{label}</label>
+        <label htmlFor={id}>{children}</label>
         &nbsp;
         <input
             id={id}
